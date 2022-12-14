0429f71fc13f36fec09c2f62a4aadc852681c534
Rodrigo <rodrigorael53@gmail.com>
2022-12-14 00:14:36 -0300

    creating the notification entity

---
+++ src/application/entities/notification.ts
@@ -0,0 +1,56 @@
+export interface NotificationProps {
+  recipientId: string;
+  content: string;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  private props: NotificationProps;
+
+  constructor(props: NotificationProps) {
+    this.props = props;
+  }
+
+  //os getters e setters são métodos que definimos dentro da entidade, são métodos
+  // para acessar um valor específico(get) dentro da entidade ou atualizar
+  // um valor específico(set)
+  // os getters e setters servem como interceptadores no momento de qualquer parte
+  // do código acessar uma propriedade ou tentar alterar o valor da propriedade
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set content(content: string) {
+    this.props.content = content;
+  }
+
+  public get content(): string {
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
