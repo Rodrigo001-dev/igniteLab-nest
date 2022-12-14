210b094d7a9324f6f8caf7e93a4ff4f39623d804
Rodrigo <rodrigorael53@gmail.com>
2022-12-13 23:37:03 -0300

    separating some files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { PrismaService } from './prisma.service';
+import { AppController } from './infra/app.controller';
+import { PrismaService } from './infra/prisma.service';
 
 @Module({
   imports: [],
