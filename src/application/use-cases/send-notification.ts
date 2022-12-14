de2767bdfd518f42941fe3787e1031d5c47f79e7
Rodrigo <rodrigorael53@gmail.com>
2022-12-14 15:57:53 -0300

    making application save the notifications in the database using the repository and the use case

--- src/application/use-cases/send-notification.ts
+++ src/application/use-cases/send-notification.ts
@@ -1,3 +1,5 @@
+import { Injectable } from '@nestjs/common';
+
 import { Content } from '../entities/content';
 import { Notification } from '../entities/notification';
 import { NotificationsRepository } from '../repositories/notifications-repository';
@@ -12,6 +14,7 @@ interface SendNotificationResponse {
   notification: Notification;
 }
 
+@Injectable()
 export class SendNotification {
   constructor(private notificationsRepository: NotificationsRepository) {}
 
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { SendNotification } from 'src/application/use-cases/send-notification';
+import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [NotificationsController],
+  providers: [SendNotification],
 })
 export class HttpModule {}
