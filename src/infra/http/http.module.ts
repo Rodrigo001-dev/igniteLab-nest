1691e33c753a04bffb70f5b79b286193c5042e59
Rodrigo <rodrigorael53@gmail.com>
2022-12-14 15:23:10 -0300

    making the structure of folders and files

---
+++ src/infra/http/http.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+
+import { NotificationsController } from './controllers/notifications.controller';
+
+@Module({
+  imports: [],
+  controllers: [NotificationsController],
+})
+export class HttpModule {}
